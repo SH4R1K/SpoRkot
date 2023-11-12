efcfbeea2ec228d3a1b748f13a7d6afc90b938e2
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2023-11-12 13:57:01 +0300

    more infoo

--- SPO_RKOT_UI/Views/MoreInfoView.xaml.cs
+++ SPO_RKOT_UI/Views/MoreInfoView.xaml.cs
@@ -31,16 +31,17 @@ namespace SPO_RKOT_UI.Views
             Close();
         }
 
-        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
-        {
-            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
-        }
         [DllImport("user32.dll")]
         public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
-        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void PanelControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             WindowInteropHelper helper = new WindowInteropHelper(this);
             SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void PanelControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
     }
 }
